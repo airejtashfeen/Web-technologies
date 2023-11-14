f0a0112508f3a92a83dac57f68b418f64be6463e
Airej Tashfeen <airejtashfeen620@gmail.com>
2023-11-14 14:04:25 +0500

    modified:   .DS_Store 	modified:   Password Generator/script.js 	new file:   Weather App/.DS_Store 	new file:   Weather App/images/clear.png 	new file:   Weather App/images/clouds.png 	new file:   Weather App/images/drizzle.png 	new file:   Weather App/images/humidity.png 	new file:   Weather App/images/mist.png 	new file:   Weather App/images/rain.png 	new file:   Weather App/images/search.png 	new file:   Weather App/images/snow.png 	new file:   Weather App/images/wind.png 	new file:   Weather App/index.html 	new file:   Weather App/script.js 	new file:   Weather App/style.css

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,33 +1,43 @@
-class Password {
-    constructor() {
-        console.log("Welcome to the password generator");
-        this.pass = "";
+class Password{
+    constructor(){
+        this.pass="";
     }
 
-    generatePassword(len) {
-        if (len < 3) {
-            return "Length of password should be at least 3 characters";
-        } else {
-            let i = 0;
-            while (i < len) {
-                this.pass += chars[Math.floor(Math.random() * chars.length)];
-                this.pass += nums[Math.floor(Math.random() * nums.length)];
-                this.pass += special[Math.floor(Math.random() * special.length)];
+   generatePassword(len){
+        if(len<3){
+            return "Your Password should be above 3 characters"
+        }
+        else{
+            let i=0;
+            while(i<len){
+                this.pass+= chars[Math.floor(Math.random()* chars.length)];
+                this.pass+= nums[Math.floor(Math.random()* nums.length)];
+                this.pass+= specials[Math.floor(Math.random()* specials.length)];
                 i+=3;
             }
-            this.pass = this.pass.substr(0, len);
+            this.pass= this.pass.substring(0, len);
             return this.pass;
         }
     }
 }
 
-let chars = "qwertyuiopasdfghjklzxcvbnm";
-let nums = "1234567890";
-let special = "!@#$%^&*()";
+let chars="qwertyuiopasdfghjklzxcvbnm";
+let nums= "1234567890";
+let specials= "!@#$%^&*()";
 
-let p = new Password();
-console.log(p.generatePassword(8));
-
+let p= new Password();
 let x= document.getElementById('resultDiv');
+
 x.append(p.generatePassword(8));
-x.style.fontSize="40px";
+x.style.fontSize='40px';
+
+
+
+
+
+
+
+
+
+
+
